0be7a6aff336d0c62154eba63b40944adca9a9d7
javieriq <javier.iniguez2498@gmail.com>
2020-05-14 13:01:30 -0500

    show post

---
+++ Application/app/src/main/java/com/itesm/covapp/models/PostModel.kt
@@ -0,0 +1,12 @@
+package com.itesm.covapp.models
+
+import java.io.Serializable
+
+class PostModel:Serializable {
+    var lastName: String? = ""
+    var username: String? = ""
+    var postContent: String? = ""
+    var title: String? = ""
+    var state: String? = ""
+    var topic: String? = ""
+}
--- Application/app/src/main/java/com/itesm/covapp/models/Salad.kt
+++
@@ -1,6 +0,0 @@
-package com.itesm.covapp.models
-
-data class Salad(
-    val name: String = "",
-    val description: String = "",
-    var uuid: String = "")
