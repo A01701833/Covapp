128e719cc3917e02420ff762ad6b2bd9ff16e015
GeraDNG <geradng@gmail.com>
2020-06-07 19:45:05 -0500

    Modificado el estilo del mapa

--- Application/app/src/main/java/com/itesm/covapp/activity_map.kt
+++ Application/app/src/main/java/com/itesm/covapp/activity_map.kt
@@ -1,9 +1,13 @@
 package com.itesm.covapp
 
+import com.itesm.covapp.R
+
 import android.content.pm.PackageManager
+import android.content.res.Resources
 import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
@@ -13,13 +17,16 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.MarkerOptions
+import java.util.*
 
 class activity_map : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var MAP: GoogleMap
     private lateinit var userLastLocation: Location
     private lateinit var userCurrentLocation : FusedLocationProviderClient
+    private val TAG = activity_map::class.java.simpleName
 
     companion object {
         private const val REQUEST_LOCATION_PERMISSION = 0
@@ -35,20 +42,50 @@ class activity_map : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap) {
         MAP = googleMap
+        MAP.isMyLocationEnabled = true
         MAP.uiSettings.isZoomGesturesEnabled = true
         MAP.uiSettings.isZoomControlsEnabled = true
-        MAP.mapType = GoogleMap.MAP_TYPE_HYBRID
-        userCurrentLocation = LocationServices.getFusedLocationProviderClient(this)
+        //MAP.mapType = GoogleMap.MAP_TYPE_TERRAIN
+        setMapStyle(MAP)
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_LOCATION_PERMISSION)
             return
         }
-        MAP.isMyLocationEnabled = true
+        userCurrentLocation = LocationServices.getFusedLocationProviderClient(this)
         userCurrentLocation.lastLocation.addOnSuccessListener(this) { location ->
             if (location != null) {
                 userLastLocation = location
-                MAP.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(location.latitude, location.longitude), 17f))
+                MAP.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(location.latitude, location.longitude), 16f))
+
+                /*
+                TODO: Aquí obtendremos los datos de firebase para colocar marcadores en el mapa para cada reporte que haya.
+                 Debemos meter lo siguiente en un FOR.
+                 Los marcadores se colocan de la siguiente forma. Sustituiremos la latitud y longitud por las coordenadas de firebase.
+                 En la etiqueta del marcador también debemos poner los datos de firebase.
+                */
+                MAP.addMarker(
+                    MarkerOptions()
+                        .position(LatLng(userLastLocation.latitude, userLastLocation.longitude))
+                        .title("Título del pin") // Por ejemplo: "Sospechoso"
+                        .snippet("Descripción del pin") // Por ejemplo: "Bla bla bla"
+                )
+
+
             }
+        }
+    }
+
+    private fun setMapStyle(map: GoogleMap) {
+        try {
+            val success = map.setMapStyle(
+                MapStyleOptions.loadRawResourceStyle(this, R.raw.estilo_mapa)
+            )
+            if (!success) {
+                Log.e(TAG, "Style error")
+            }
+        }
+        catch (e: Resources.NotFoundException) {
+            Log.e(TAG, "No se encontró el estilo. Error: ", e)
         }
     }
 }
